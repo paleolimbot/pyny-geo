2b1b7294b10625f5ca93fa9b6bae869380cdff3b
Dewey Dunnington <dewey@fishandwhistle.net>
2015-10-28 16:09:40 -0300

    clean and update relevance

--- src/nmealib/serialuitl.py
+++
@@ -1,8 +0,0 @@
-import serial
-import glob
-
-def scanPortsLinux():
-    """scan for available ports. return a list of device names."""
-    return glob.glob('/dev/ttyS*') + glob.glob('/dev/ttyUSB*') + glob.glob('/dev/tty.usb*')
-
-print scanPortsLinux()
